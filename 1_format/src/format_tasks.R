226e4d23f5a2a12da6fc716f853dd80743939245
wdwatkins <wwatkins@usgs.gov>
2018-09-27 17:12:38 -0500

    fully connect pipeline

--- 1_format/src/format_tasks.R
+++ 1_format/src/format_tasks.R
@@ -1,10 +1,12 @@
 lookup_meteo_file <- function(site_id) {
-  #once the table exists, will lookup there
-  sprintf("in/driver-data/%s_meteo.feather.ind", site_id)
+  #how should the dependency on the lookup table be handled here?
+  table <- readRDS("in/feature_nldas_coords.rds")
+  lake_row <- filter(table, !!site_id == site_id)
+  sprintf("in/driver-data/NLDAS_time[0.346848]_x[%s]_y[%s].csv", lake_row$nldas_coord_x, lake_row$nldas_coord_y)
 }
 
 get_input_file_names <- function(site_id = task_name) {
-  obs_file <- sprintf("1_format/tmp/%s_separated_obs.feather.ind", site_id)
+  obs_file <- sprintf("1_format/tmp/%s_separated_obs.rds.ind", site_id)
   glm_preds_file <- sprintf('1_format/in/glm_preds/%s_output.nc.ind', site_id)
   meteo_file <- lookup_meteo_file(site_id)
   return(list(obs_file=obs_file, glm_preds_file=glm_preds_file,
@@ -12,19 +14,29 @@ get_input_file_names <- function(site_id = task_name) {
 }
 
 #assume task_name == site_id
-create_format_task_plan <- function(site_ids, ind_dir, settings_yml = "lib/cfg/settings.yml") {
-  # combine_nn_data has 3 .ind files as arguments but right now only uses them
-  # to determine the names of data files, which I promise exist. These are really
-  # just placeholders until we can connect up the start of this pipeline so that
-  # data files come from other pipelines, at which point the I() around those .ind
-  # files will no longer be appropriate and we should call sc_retrieve within
-  # combine_nn_data.
+create_format_task_plan <- function(priority_lakes, ind_dir, settings_yml = "lib/cfg/settings.yml") {
   task_steps <- list(
+    create_task_step(
+      step_name = "yeti_download_ind",
+      target_name = function(task_name, ...) {
+        sprintf("1_format/in/glm_preds/%s_output.nc.ind", task_name)
+      },
+      command = "syncr_indicate(out_ind = target_name, glm_yeti_path = glm_yeti_path_cfg)"
+    ),
+    create_task_step(
+      step_name = "yeti_download_nc",
+      target_name = function(steps, ...) {
+        as_data_file(steps[['yeti_download_ind']]$target_name)
+      },
+      command = function(steps, ...) {
+        sprintf("require_local('%s')", steps[['yeti_download_ind']]$target_name)
+      }
+    ),
     # step1a_separate_obs_ind
     create_task_step(
       step_name = "separate_obs_ind",
       target_name = function(task_name, ...) {
-        sprintf("1_format/tmp/%s_separated_obs.feather.ind", task_name)
+        sprintf("1_format/tmp/%s_separated_obs.rds.ind", task_name)
       },
       command = function(task_name, ...) {
         sprintf("separate_obs(out_ind = target_name, site_id = I(\'%s\'))", task_name)
@@ -49,9 +61,9 @@ create_format_task_plan <- function(site_ids, ind_dir, settings_yml = "lib/cfg/s
         psprintf(
           "combine_nn_data(",
           "combined_ind = target_name,",
-          "obs_ind = I('%s')," = obs_file, # we should take away the I() if we ever actually have these .ind files
-          "glm_preds_ind = I('%s')," = glm_preds_file,
-          "meteo_ind = I('%s')," = meteo_file,
+          "obs_ind = '%s'," = obs_file, # we should take away the I() if we ever actually have these .ind files
+          "glm_preds_ind = '%s'," = glm_preds_file,
+          "meteo = '%s'," = meteo_file,
           "combine_cfg = combine_cfg)")
       }),
     # step2b_combine_rds
@@ -104,7 +116,7 @@ create_format_task_plan <- function(site_ids, ind_dir, settings_yml = "lib/cfg/s
   )
 
   format_task_plan <- create_task_plan(
-    task_names = site_ids,
+    task_names = priority_lakes$site_id,
     task_steps = task_steps,
     final_steps = "split_scale_ind",
     ind_dir = "1_format/log",
@@ -123,9 +135,19 @@ create_format_task_makefile <- function(task_plan, makefile, ...) {
     ...)
 }
 
-separate_obs <- function(site_id, out_ind, all_obs_file = "in/merged_temp_data_daily.feather") {
+separate_obs <- function(site_id, out_ind, all_obs_file_ind = "in/merged_temp_data_daily.feather.ind") {
   #assuming data is already fully clean
-  site_obs <- read_feather(all_obs_file) %>% filter(.data$site_id == site_id)
-  write_feather(x = site_obs, path = as_data_file(out_ind))
+  site_obs <- read_feather(as_data_file(all_obs_file_ind)) %>% filter(nhd_id == site_id) %>%
+    rename(DateTime = date) %>% as.data.frame() #need these col names for glmtools::resample_to_field
+  saveRDS(object = site_obs, file = as_data_file(out_ind))
   sc_indicate(ind_file = out_ind, data_file = as_data_file(out_ind))
+}
+
+syncr_indicate <- function(out_ind, glm_yeti_path) {
+  user <- Sys.info()[['user']] #assumes this is your yeti login
+  out_data_file <- as_data_file(out_ind)
+  src <- sprintf('%s@yeti.cr.usgs.gov:%s/%s', user, glm_yeti_path, basename(out_data_file))
+  dest <- sprintf('1_format/in/glm_preds/%s_output.nc', site_id)
+  syncr(src, dest)
+  sc_indicate(ind_file = out_ind, data_file = out_data_file)
 }
